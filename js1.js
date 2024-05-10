df68baf50193d5b32486083636d0d62f0ffbc6a6
Nihal Karkera <155234818+nihalkarkeraa@users.noreply.github.com>
2024-05-10 07:22:04 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,17 @@
+console.log("currency converter")
+console.log("--------------------");
+console.log("convert from international currency to indian currency");
+console.log("--------------------");
+let choice=prompt("Enter 1 for INT to INR \n Enter 2 for INT to INR");
+if(choice==1){
+let subschoice=prompt("1.USD \n 2.JPY \n 3.CAD \n 4.RBL 5.EURO");
+if(subschoice==1){
+    n=prompt("enter the Amount in USD:")
+    finalconv=n/83.84;
+    console.log(finalconv)
+}
+
+}
+
+let inr=prompt("enter amount of ruppeee");
+
